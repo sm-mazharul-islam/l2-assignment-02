cd62b2d8045bf66076ecdf695daeb75fce49bd50
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2023-11-25 18:59:29 +0600

    commited

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -13,7 +13,6 @@ export type TOrders = {
   productName: string;
   price: number;
   quantity: string;
-  // _id: false; ---> _id hide korte
 };
 
 export type TUser = {
@@ -35,7 +34,3 @@ export interface UserModel extends Model<TUser> {
   // eslint-disable-next-line no-unused-vars
   isUserExists(userId: number): Promise<TUser | null>;
 }
-
-// export interface UserModel extends Model<TUser> {
-//   isUserExists(userId: number): Promise<TUser | null>;
-// }
